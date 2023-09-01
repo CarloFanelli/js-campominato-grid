5e014e711cd9dae6040f8dd529bf006e2bb3437c
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-01 16:18:21 +0200

    add function on click of the square

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,49 @@
-function createField(elementDOM,squareNumber) {
-    
+function createField(elementDOM, squareNumber) {
+
     let i = 0;
     while (i < squareNumber) {
 
-        const squareMarkup = `<div class="square">${i+1}</div>`
+        const squareMarkup = `<div class="square">${i + 1}</div>`;
 
-        document.querySelector('.field').insertAdjacentHTML('beforeend',squareMarkup);
-        
-console.log('ciao');
+        //document.querySelector('.field').insertAdjacentHTML('beforeend', squareMarkup);
+
+        //document.querySelector('.field').innerHTML += squareMarkup;
+
+        const squareDOM = document.createElement('div');
+
+        const squareTag = document.createElement('h1')
+
+        const squareValue = i +1;
+
+        squareTag.append(squareValue);
+
+        squareDOM.append(squareTag);
+
+        squareDOM.classList.add('square');
+        squareTag.classList.add('d-none')
+
+        elementDOM.append(squareDOM);
+
+
+
+        squareDOM.addEventListener('click', function () {
+
+            console.log(squareValue);
+
+            squareDOM.classList.toggle('bg-info')
+
+            squareTag.classList.toggle('d-none')
+        })
 
         i++;
     }
 
 }
 
-document.getElementById('create-field').addEventListener('click',function(){
-    
+document.getElementById('create-field').addEventListener('click', function () {
+
     const fieldDOM = document.querySelector('.field');
-    
-    createField(fieldDOM,100);
-    
+
+    createField(fieldDOM, 100);
+
 })
